3795fd1c89da549d1ad399372a2cdc65210b0857
Onur Eker <yonureker@gmail.com>
2017-11-18 19:30:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function printBadges(myArray){
+  for (let i = 0; i < myArray.length ; i++){
+    console.log(`Welcome ${myArray[i]}! You are employee #${i+1}.`);
+  }
+  return myArray;
+}
+
+printBadges();
+
+
+function theFlip(){
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let x = 0;
+  while (theFlip()) {
+    x++;
+  }
+  return(`You got ${x} tails in a row!`);
+}
