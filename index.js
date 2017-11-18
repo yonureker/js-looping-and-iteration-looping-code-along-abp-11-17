d5ac7d0eb9c63886ea3f1947ab4a41a7c113a711
Onur Eker <yonureker@gmail.com>
2017-11-18 19:03:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function printBadges(myArray){
+  for (let i = 0; i < myArray.length ; i++){
+    console.log(`Welcome ${myArray[i]}! You are employee #${i+1}.`);
+  }
+  return myArray;
+}
+
+printBadges();
+
+
+function theFlip(){
+  return Math.random() >= 0.1;
+}
+
+function tailsNeverFails(){
+  let x = 0;
+  while (theFlip()) {
+    x++;
+  }
+  return(`You got ${x} tails in a row!`);
+}
+
+tailsNeverFails();
